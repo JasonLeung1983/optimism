e132df93d3429450e1a8ef3feeb6c1e8d6ad7cf5
Kelvin Fichter <kelvinfichter@gmail.com>
2025-09-19 23:17:33 -0400

    feat: OPCMv2

--- packages/contracts-bedrock/scripts/libraries/Config.sol
+++ packages/contracts-bedrock/scripts/libraries/Config.sol
@@ -250,4 +250,9 @@ library Config {
     function devFeatureDeployV2DisputeGames() internal view returns (bool) {
         return vm.envOr("DEV_FEATURE__DEPLOY_V2_DISPUTE_GAMES", false);
     }
+
+    /// @notice Returns true if the development feature opcm_v2 is enabled.
+    function devFeatureOpcmV2() internal view returns (bool) {
+        return vm.envOr("DEV_FEATURE__OPCM_V2", false);
+    }
 }
